d8998ae5842911c7688cbcad196740a68155fbe1
Caleb Smith <karagenit@outlook.com>
2017-12-14 21:23:45 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# First, extract the tarball, like 'tar -xvzf Autodesk_Eagle.tar.gz'
+# This will give you a new directory like 'eagle-8.5.0'
+# Call this script (from this repo - it needs the .desktop file found here) with 
+# the extracted directory name as it's only argument.
+
+files=$1
+
+# Moves the program to a proper location
+mv "$files" "/usr/local/lib/"
+
+# Links the executable to your path
+ln -s "/usr/local/lib/$files" "/usr/local/bin/eagle"
+
+# Allows Window Managers like GNOME to find Eagle
+mv "./eagle.desktop" "/usr/share/applications/"
